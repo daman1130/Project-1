b61ed2f6deb5621d4436eb4427a817386a597788
daman1130 <154652092+daman1130@users.noreply.github.com>
2023-12-22 20:32:25 -0800

    Add files via upload

---
+++ christmas.js
@@ -0,0 +1,16 @@
+const christmasColors = ["#B22222", "#228B22", "#FFFAFA", "#FFD700"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = christmasColors[randomNumber];
+    color.textContent = christmasColors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * christmasColors.length);
+}
+
+
